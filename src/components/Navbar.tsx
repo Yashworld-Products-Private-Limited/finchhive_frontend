75b7a4d3250de2db3ad36c62951fd6bb5825df1d
jeelYWPPL <jeel.frontend.dev@yashworldproducts.in>
2026-05-18 18:17:44 +0530

    Trigger Vercel deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const navLinks = [
   { label: "Services", href: "/#services" },
   { label: "Testimonials", href: "/#testimonials" },
   { label: "Case Studies", href: "/#case-studies" },
-  { label: "Our Team Members", href: "/about#our-team" },
+  { label: "Our Team", href: "/about#our-team" },
 ];
 
 export default function Navbar() {
