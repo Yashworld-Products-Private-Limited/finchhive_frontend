4ee0c8a141d241427efcb1b3160d48d4afceb347
administrator <admin>
2026-07-20 22:01:22 +0530

    R2 test2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -204,8 +204,8 @@ const AboutPage = () => {
               `}
                   />
                   <Image
-                    // src="/api/images/jems.png"
-                    src="/api/finchhivlogo.png"
+                    src="/api/imgs/jems.png"
+                    // src="/api/finchhivlogo.png"
                     alt="leader"
                     width={400}
                     height={500}
