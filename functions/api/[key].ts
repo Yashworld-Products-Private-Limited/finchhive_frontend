d9bd424159a9cca1e1ba40b59fa4f5cf8e4b097e
administrator <admin>
2026-07-20 21:56:45 +0530

    updates with ci cd testing

--- functions/api/[key].ts
+++ functions/api/[key].ts
@@ -19,6 +19,9 @@ export async function onRequestGet(context: {
   if (!object) {
     return new Response("Not found", {
       status: 404,
+      headers: {
+        "cache-control": "no-store",
+      },
     });
   }
 
